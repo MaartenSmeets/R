6823f82fb2398bb8156fba8ec9acef20e603b089
r.w.w.brouwer <r.w.w.brouwer@gmail.com>
2017-03-22 21:20:45 +0100

    added random forest install

--- basic_install.R
+++ basic_install.R
@@ -9,3 +9,10 @@
 # packages to install
 install.packages(c("ggplot2", "plyr", "reshape2"))
 
+# to get the information for a package
+library(help = "base")
+
+# information about a function
+?read.table
+
+install.packages(c("randomForest"))
