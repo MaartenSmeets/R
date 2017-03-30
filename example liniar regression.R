ae5bc74f3aba1b0eebb41afdeb1621fef4ff3696
MaartenSmeets <a7HUQbz6mDzfsGHA3peC>
2017-03-30 19:48:30 +0200

    Adding liniar regression sample

---
+++ example liniar regression.R
@@ -0,0 +1,23 @@
+library(ggplot2)
+require(plyr)
+
+alligator = data.frame(
+  lnLength = c(3.87, 3.61, 4.33, 3.43, 3.81, 3.83, 3.46, 3.76,
+               3.50, 3.58, 4.19, 3.78, 3.71, 3.73, 3.78),
+  lnWeight = c(4.87, 3.93, 6.46, 3.33, 4.38, 4.70, 3.50, 4.50,
+               3.58, 3.64, 5.90, 4.43, 4.38, 4.42, 4.25)
+)
+
+ggplotRegression <- function (fit) {
+  ggplot(fit$model, aes_string(x = names(fit$model)[2], y = names(fit$model)[1])) + 
+    geom_point() +
+    stat_smooth(method = "lm", col = "red") +
+    labs(title = paste("Adj R2 = ",signif(summary(fit)$adj.r.squared, 5),
+                       "Intercept =",signif(fit$coef[[1]],5 ),
+                       " Slope =",signif(fit$coef[[2]], 5),
+                       " P =",signif(summary(fit)$coef[2,4], 5)))
+}
+
+alli.mod1 = lm(lnWeight ~ lnLength, data = alligator)
+ggplotRegression(alli.mod1)
+
